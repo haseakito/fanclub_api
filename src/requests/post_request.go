eda8b57c017d1f5cf85355241b5146f1e70c7426
haseakito <haseakito@gmail.com>
2024-03-03 01:49:55 +1100

    :safety_vest: Add `post_request` to validate `Post` request

---
+++ src/requests/post_request.go
@@ -0,0 +1,46 @@
+package requests
+
+import (
+	"github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type PostRequest struct {
+	Title           string   `json:"title"`
+	UserID          string   `json:"user_id"`
+	Description     *string  `json:"description"`
+	Price           *int     `json:"price"`
+	CategoryIDs     []string `json:"categories"`
+	SubscriptionIDs []string `json:"subscriptions"`
+	IsFeatured      bool     `json:"is_featured"`
+	Status          bool     `json:"status"`
+}
+
+func (req PostRequest) Validate() error {
+	return validation.ValidateStruct(&req,
+		validation.Field(
+			&req.Title,
+			validation.Required,
+			validation.Length(1, 100),
+		),
+		validation.Field(
+			&req.UserID,
+			validation.Required,
+		),
+		validation.Field(
+			&req.Description,
+			validation.Length(0, 1000),
+		),
+		validation.Field(
+			&req.Price,
+			validation.Min(0),
+		),
+		validation.Field(
+			&req.CategoryIDs,
+			validation.Each(validation.Length(1, 100)),
+		),
+		validation.Field(
+			&req.SubscriptionIDs,
+			validation.Each(validation.Length(1, 100)),
+		),
+	)
+}
