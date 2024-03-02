4964619777e9f8de5d02e12080b7c471eb1e189d
haseakito <haseakito@gmail.com>
2024-03-03 01:23:13 +1100

    :sparkles: Add `Post`, `Category` and `Subscription` schema

---
+++ src/ent/schema/category.go
@@ -0,0 +1,34 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Category holds the schema definition for the Category entity.
+type Category struct {
+	ent.Schema
+}
+
+// Fields of the Category.
+func (Category) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New),
+		field.String("name"),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the Category.
+func (Category) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("posts", Post.Type).Ref("categories"),
+	}
+}
---
+++ src/ent/schema/post.go
@@ -0,0 +1,43 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Post holds the schema definition for the Post entity.
+type Post struct {
+	ent.Schema
+}
+
+// Fields of the Post.
+func (Post) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New),
+		field.String("user_id"),
+		field.String("title"),
+		field.Text("description").Optional(),
+		field.Int("price").Optional(),
+		field.Bool("is_featured").Default(false).Nillable(),
+		field.Bool("status").Default(false).Nillable(),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the Post.
+func (Post) Edges() []ent.Edge {
+	return []ent.Edge{
+		// Has-many relationship to category
+		edge.To("categories", Category.Type),
+
+		// Belongs-to relationship to subscription
+		edge.From("subscriptions", Subscription.Type).Ref("posts"),
+	}
+}
