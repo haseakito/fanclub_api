41492314a19c8a1de01a009dda3de9e143911e4a
haseakito <haseakito@gmail.com>
2024-03-08 15:03:09 +1100

    :white_check_mark: Add unit test for `User` request

---
+++ src/tests/requests/user_request_test.go
@@ -0,0 +1,42 @@
+package requests_test
+
+import (
+	"testing"
+
+	"github.com/hackgame-org/fanclub_api/requests"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserRequest(t *testing.T) {
+	// Instantiate a valid request
+	validReq := requests.UserRequest{
+		Username: "user-123",
+		Bio:      "test user description",
+		Url:      "http://twitter.com/",
+	}
+
+	// Validate valid request
+	err := validReq.Validate()
+	assert.NoError(t, err)
+
+	// valid request: missing username
+	validReq = requests.UserRequest{
+		Bio:      "test user description",
+		Url:      "http://twitter.com/",
+	}
+
+	// Validate valid request
+	err = validReq.Validate()
+	assert.NoError(t, err)
+	
+	invalidReq := requests.UserRequest{
+		Username: "user-123",
+		Bio:      "test user description",
+		Url:      "not a url",
+	}
+
+	// Validate invalid request
+	err = invalidReq.Validate()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "url")
+}
