c9bbaad1b0696c6e7321483716ecef5463a4bbde
haseakito <haseakito@gmail.com>
2024-05-06 12:52:05 +1000

    :wrench: Set up config file

---
+++ src/internal/database/database.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/hackgame-org/fanclub_api/api/ent"
+)
+
+func Initialize() (*ent.Client, error) {
+	// Establish database connection and initialize Ent client
+	client, err := ent.Open("mysql", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Failed opening connection to mysql: %v", err)
+	}
+
+	// Run the auto migration
+	if err := client.Schema.Create(context.Background()); err != nil {
+		defer client.Close()
+		log.Fatalf("Failed creating schema resources: %v", err)
+	}
+
+	return client, nil
+}
