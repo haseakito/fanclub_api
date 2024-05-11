77ed9f4315959270573c0392df58a4e9dbd6f9a9
haseakito <haseakito@gmail.com>
2024-05-11 14:44:25 +1000

    :sparkles: Update user request

--- src/api/requests/user_request.go
+++ src/api/requests/user_request.go
@@ -5,16 +5,16 @@ import (
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
-type UserRequest struct {
-	Username string `json:"username"`
-	Bio      string `json:"bio"`
-	Url      string `json:"url"`
+type ProfileUpdateRequest struct {
+	Name string `json:"name"`
+	Bio  string `json:"bio"`
+	Url  string `json:"url"`
 }
 
-func (req UserRequest) Validate() error {
+func (req ProfileUpdateRequest) Validate() error {
 	return validation.ValidateStruct(&req,
 		validation.Field(
-			&req.Username,
+			&req.Name,
 			validation.Length(1, 100),
 		),
 		validation.Field(
@@ -24,6 +24,31 @@ func (req UserRequest) Validate() error {
 		validation.Field(
 			&req.Url,
 			is.URL,
+		),
+	)
+}
+
+type AcountUpdateRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	DOB      string `json:"dob"`
+}
+
+func (req AcountUpdateRequest) Validate() error {
+	return validation.ValidateStruct(&req,
+		validation.Field(
+			&req.Username,
+			validation.Length(1, 100),
+		),
+		validation.Field(
+			&req.Email,
+			validation.Required,
+			validation.Length(1, 100),
+			is.Email.Error("Invalid email format"),
+		),
+		validation.Field(
+			&req.DOB,
+			validation.Required,
 		),
 	)
 }
