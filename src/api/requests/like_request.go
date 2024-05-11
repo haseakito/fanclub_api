c0ad4dfeafa4f438831bd09c209c2dc947e051ba
haseakito <haseakito@gmail.com>
2024-05-11 14:45:29 +1000

    :sparkles: Update like request

--- src/api/requests/like_request.go
+++ src/api/requests/like_request.go
@@ -3,16 +3,11 @@ package requests
 import "github.com/go-ozzo/ozzo-validation/v4"
 
 type LikeRequest struct {
-	UserID string `json:"user_id"`
-	PostID string `json:"post_id"`
+	PostID string `json:"postId"`
 }
 
 func (req LikeRequest) Validate() error {
 	return validation.ValidateStruct(&req,
-		validation.Field(
-			&req.UserID,
-			validation.Required,
-		),
 		validation.Field(
 			&req.PostID,
 			validation.Required,
